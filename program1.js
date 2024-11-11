892ac645b44becd2a94f12c625e79c20b60ba980
DACKaladhar <darck21042004@gmail.com>
2024-11-11 15:30:36 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,32 +2,31 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-        const stack = [];
-        const openBrackets = '([{';
-        const closeBrackets = ')]}';
-        const matchingBrackets = {
-            ')': '(',
-            ']': '[',
-            '}': '{'
-        };
+var isValid = function (s) {
+  const stack = [];
+  const openBrackets = '([{';
+  const closeBrackets = ')]}';
+  const matchingBrackets = {
+    ')': '(',
+    ']': '[',
+    '}': '{'
+  };
 
-        for (const e of s) {
-            if (openBrackets.includes(e)) {
-                stack.push(e);
-            } else if (closeBrackets.includes(e)) {
-                if (stack.length === 0) {
-                    return false;
-                } else if (stack[stack.length - 1] !== matchingBrackets[e]) {
-                    return false;
-                } else {
-                    stack.pop();
-                }
-            }
-        }
-
-        return stack.length === 0;
+  for (const e of s) {
+    if (openBrackets.includes(e)) {
+      stack.push(e);
+    } else if (closeBrackets.includes(e)) {
+      if (stack.length === 0) {
+        return false;
+      } else if (stack[stack.length - 1] !== matchingBrackets[e]) {
+        return false;
+      } else {
+        stack.pop();
+      }
     }
+  }
+
+  return stack.length === 0;
 
 };
 

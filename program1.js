5af495d45482b5a52ea74010034cda2b604aa678
DACKaladhar <darck21042004@gmail.com>
2024-11-11 15:30:09 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,23 +3,7 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-  class Solution(object):
-  def isValid(self, s):
-      stack = []
-      for e in s:
-          if e in '[{(':
-              stack.append(e)
-          else:
-              if not stack:
-                  return False
-              elif (e == ')' and stack[-1] != '(') or (e == ']' and stack[-1] != '[') or (e == '}' and stack[-1] != '{'):
-                  return False
-              else:
-                  stack.pop()
-      
-      if stack:
-          return False
-      return True
+    
 };
 
 module.exports = { isValid };

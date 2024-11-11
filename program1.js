b691cbdbba6dbde5c7cacd09aa49565d2e3c8ecb
DACKaladhar <darck21042004@gmail.com>
2024-11-11 15:30:28 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -30,13 +30,6 @@ var isValid = function(s) {
 
         return stack.length === 0;
     }
-}
-
-// Example usage
-const solution = new Solution();
-const input = "{[()]}"; // Change this value to test different inputs
-const result = solution.isValid(input);
-console.log(`The string '${input}' is ${result ? 'valid' : 'invalid'}.`);
 
 };
 
